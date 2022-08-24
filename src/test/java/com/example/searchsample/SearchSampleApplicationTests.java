df605b027b27f1117327bb58c3f4c848ed065696
jongjoo kim <jongjoo.kim@hanwha.com>
2022-08-24 22:55:55 +0900

    feat: second commit

---
+++ src/test/java/com/example/searchsample/SearchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
