1f6bb373279dadb1253623c24e673e24ae22cfce
jongjoo kim <jongjoo.kim@hanwha.com>
2022-08-29 22:25:28 +0900

    refact: 다른 검색 API 추가 가능하게 리팩토링

--- src/main/java/com/example/searchsample/search/service/SearchPlaceService.java
+++ src/main/java/com/example/searchsample/search/service/SearchPlaceService.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 주소 검색에 대한 기능
@@ -27,35 +28,55 @@ import java.util.Queue;
 @RequiredArgsConstructor
 public class SearchPlaceService {
 
+    private static final int RESULT_MAX_SIZE = 10;
+
     private final KakaoPlaceService kakaoPlaceService;
     private final NaverPlaceService naverPlaceService;
     private final SearchHistoryService searchHistoryService;
 
-
     /**
      * 검색을 위한 기능
-     * 타이틀을 입력받아서 네이버, 카카오의 응답을 논블럭킹 방식으로 송수신.
+     * 타이틀을 입력받아서 장소 검색에 대한 n개 응답을 논블럭킹 방식으로 송수신.
      *
      * @param name 검색어
      * @return SearchPlaceResponse
      */
     public SearchPlaceResponse search(String name) {
+        List<Queue<PlaceDto>> queueList = new ArrayList<>();
         searchHistoryService.saveSearchWord(name);
         var fetchResponse = fetchSearch(name).collectList().block();
-        List<PlaceDto> kakaoPlaceDtoList = null;
-        List<PlaceDto> naverPlaceDtoList = null;
+
         if (!ObjectUtils.isEmpty(fetchResponse)) {
             for (PlaceResultDto placeDtoList : fetchResponse) {
-                if (DevSiteCode.KAKAO.equals(placeDtoList.devSiteCode())) {
-                    kakaoPlaceDtoList = placeDtoList.placeDtoList();
-                }
-                if (DevSiteCode.NAVER.equals(placeDtoList.devSiteCode())) {
-                    naverPlaceDtoList = placeDtoList.placeDtoList();
-                }
+                composeSearchApiSite(placeDtoList, queueList);
             }
         }
-        var searchPlaceList = searchResult(kakaoPlaceDtoList, naverPlaceDtoList);
+        var searchPlaceList = searchResult(queueList);
         return new SearchPlaceResponse(searchPlaceList);
+    }
+
+    /**
+     * 응답을 구성할 사이트 셋팅.
+     *
+     * @param placeDtoList 장소 API Response Dto
+     * @param queueList 장소 API Response dto List
+     */
+    private void composeSearchApiSite(PlaceResultDto placeDtoList, List<Queue<PlaceDto>> queueList) {
+        if (DevSiteCode.KAKAO.equals(placeDtoList.devSiteCode())) {
+            var kakaoPlaceDtoList = placeDtoList.placeDtoList();
+            if (!CollectionUtils.isEmpty(placeDtoList.placeDtoList())) {
+                queueList.add(new LinkedList<>(kakaoPlaceDtoList));
+            }
+        }
+
+        if (DevSiteCode.NAVER.equals(placeDtoList.devSiteCode())) {
+            var naverPlaceDtoList = placeDtoList.placeDtoList();
+            if (!CollectionUtils.isEmpty(placeDtoList.placeDtoList())) {
+                queueList.add(new LinkedList<>(naverPlaceDtoList));
+            }
+        }
+
+        // ... other api
     }
 
     /**
@@ -74,78 +95,94 @@ public class SearchPlaceService {
     /**
      * 검색 진행
      *
-     * @param kakaoPlaceDtoList 카카오 장소 dto List
-     * @param naverPlaceDtoList 네이버 장소 dto List
+     * @param queueList 장소 API Response dto List
      * @return List<SearchPlace>
      */
-    private List<SearchPlace> searchResult(List<PlaceDto> kakaoPlaceDtoList, List<PlaceDto> naverPlaceDtoList) {
-        List<SearchPlace> resultList = new ArrayList<>();
-        Queue<PlaceDto> kakaoQueue = new LinkedList<>();
-        Queue<PlaceDto> naverQueue = new LinkedList<>();
-        if (!CollectionUtils.isEmpty(kakaoPlaceDtoList)) {
-            kakaoQueue = new LinkedList<>(kakaoPlaceDtoList);
-        }
-        if (!CollectionUtils.isEmpty(naverPlaceDtoList)) {
-            naverQueue = new LinkedList<>(naverPlaceDtoList);
-        }
-        similarSearchPlace(resultList, kakaoQueue, naverQueue);
-        extraSearchPlace(resultList, kakaoQueue, naverQueue);
+    private List<SearchPlace> searchResult(List<Queue<PlaceDto>> queueList) {
+        List<SearchPlace> resultList = similarSearchPlace(queueList);
+        extraSearchPlace(resultList, queueList);
         return resultList;
     }
 
     /**
-     * 카카오 장소를 기준으로 네이버 장소들을 순환.
+     * n개의 응답을 회기하여 모든 경우의 수 구성
      * 유사한 장소가 나왔을 결우 결과 dto에 구성하며 queue에서 제거.
      *
-     * @param resultList 결과 dto
-     * @param kakaoQueue 카카오 장소 queue
-     * @param naverQueue 네이버 장소 queue
+     * @param queueList 장소 API Response dto List
+     * @return List<SearchPlace>
      */
-    private void similarSearchPlace(List<SearchPlace> resultList, Queue<PlaceDto> kakaoQueue, Queue<PlaceDto> naverQueue) {
-        for (int i = 0; i < kakaoQueue.size(); i++) {
-            PlaceDto kakaoElement = kakaoQueue.poll();
-            int naverPlaceSize = naverQueue.size();
-            for (int j = 0; j < naverPlaceSize; j++) {
-                PlaceDto naverElement = naverQueue.poll();
-                if (isSamePlace(kakaoElement, naverElement)) {
-                    SearchPlace searchPlace =
-                            new SearchPlace(kakaoElement.placeName(), kakaoElement.address(), kakaoElement.roadAddress(), kakaoElement.phone());
-                    resultList.add(searchPlace);
+    private List<SearchPlace> similarSearchPlace(List<Queue<PlaceDto>> queueList) {
+        List<PlaceDto> resultList = new ArrayList<>();
+        for (int i = 0; i + 1 < queueList.size(); i++) {
+            similarSearchPlaceDfs(resultList, queueList, i, i + 1);
+        }
+        return builderSearchPlaceList(resultList);
+    }
+
+    private void similarSearchPlaceDfs(List<PlaceDto> resultList, List<Queue<PlaceDto>> queueList, int index, int target) {
+        if (queueList.size() == target) {
+            return;
+        }
+        Queue<PlaceDto> firstQueue = queueList.get(index);
+        Queue<PlaceDto> secondQueue = queueList.get(target);
+        for (int i = 0; i < firstQueue.size(); i++) {
+            PlaceDto firstPlaceDto = firstQueue.poll();
+            int size = secondQueue.size();
+            for (int j = 0; j < size; j++) {
+                PlaceDto secondPlaceDto = secondQueue.poll();
+                if (isSamePlace(firstPlaceDto, secondPlaceDto)) {
+                    resultList.add(firstPlaceDto);
+                    j = 10;
                 } else {
-                    naverQueue.offer(naverElement);
+                    secondQueue.offer(secondPlaceDto);
                 }
             }
-            if (naverPlaceSize == naverQueue.size()) {
-                kakaoQueue.offer(kakaoElement);
+            if (size == secondQueue.size()) {
+                firstQueue.offer(firstPlaceDto);
             }
         }
+        similarSearchPlaceDfs(resultList, queueList, index, target + 1);
     }
 
     /**
-     * 유사하지 않은 장소를 카카오, 네이버 순대로 나열
+     * 유사하지 않은 장소를 검색 API를 설정한 순서대로 동적 구성.
+     * ex) kakao, naver, google 순으로 적재.
      *
      * @param resultList 결과 dto
-     * @param kakaoQueue 카카오 장소 queue
-     * @param naverQueue 네이버 장소 queue
+     * @param queueList  API 검색 결과 List<Queue<PlaceDto>>
      */
-    private void extraSearchPlace(List<SearchPlace> resultList, Queue<PlaceDto> kakaoQueue, Queue<PlaceDto> naverQueue) {
-        while (resultList.size() < 10 && (!kakaoQueue.isEmpty() || !naverQueue.isEmpty())) {
-            if (!kakaoQueue.isEmpty()) {
-                var kakaoElement = kakaoQueue.poll();
-                SearchPlace searchPlace =
-                        new SearchPlace(kakaoElement.placeName(), kakaoElement.address(), kakaoElement.roadAddress(), kakaoElement.phone());
-                resultList.add(searchPlace);
-            }
-            if (resultList.size() >= 10) {
-                break;
-            }
-            if (!naverQueue.isEmpty()) {
-                var naverElement = naverQueue.poll();
-                SearchPlace searchPlace =
-                        new SearchPlace(naverElement.placeName(), naverElement.address(), naverElement.roadAddress(), naverElement.phone());
-                resultList.add(searchPlace);
-            }
+    private void extraSearchPlace(List<SearchPlace> resultList, List<Queue<PlaceDto>> queueList) {
+        AtomicInteger count = new AtomicInteger(3);
+        while (isResultSize(resultList, count)) {
+            count.set(0);
+            queueList.forEach(queue -> {
+                        if (isAddResultList(resultList, queue)) {
+                            var element = queue.poll();
+                            SearchPlace searchPlace =
+                                    new SearchPlace(element.placeName(), element.address(), element.roadAddress(), element.phone());
+                            resultList.add(searchPlace);
+                            count.getAndIncrement();
+                        }
+                    }
+            );
         }
+    }
+
+    private List<SearchPlace> builderSearchPlaceList(List<PlaceDto> resultList) {
+        List<SearchPlace> searchPlaceList = new ArrayList<>();
+        resultList.forEach(placeDto -> {
+            var searchPlace = new SearchPlace(placeDto.placeName(), placeDto.address(), placeDto.roadAddress(), placeDto.phone());
+            searchPlaceList.add(searchPlace);
+        });
+        return searchPlaceList;
+    }
+
+    private boolean isResultSize(List<SearchPlace> resultList, AtomicInteger count) {
+        return resultList.size() < RESULT_MAX_SIZE && count.intValue() > 0;
+    }
+
+    private boolean isAddResultList(List<SearchPlace> resultList, Queue<PlaceDto> queue) {
+        return !queue.isEmpty() && resultList.size() < RESULT_MAX_SIZE;
     }
 
     /**
@@ -227,7 +264,7 @@ public class SearchPlaceService {
     }
 
     private boolean isSimilar(String first, String second) {
-        return second.contains(first);
+        return second.contains(first) || first.contains(second);
     }
 
     private <T> int getMinSize(List<T> first, List<T> second) {
