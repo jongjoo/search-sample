49b6951dcbf764601b5c6bfc50ad4fc98e6ebae8
jongjoo kim <jongjoo.kim@hanwha.com>
2022-08-29 20:13:25 +0900

    feat: javadoc

--- src/main/java/com/example/searchsample/search/dto/PlaceDto.java
+++ src/main/java/com/example/searchsample/search/dto/PlaceDto.java
@@ -1,5 +1,8 @@
 package com.example.searchsample.search.dto;
 
+/**
+ * The type Place dto.
+ */
 public record PlaceDto(
         String placeName,
         String address,
--- src/main/java/com/example/searchsample/search/dto/PlaceResultDto.java
+++ src/main/java/com/example/searchsample/search/dto/PlaceResultDto.java
@@ -4,6 +4,9 @@ import com.example.searchsample.common.code.DevSiteCode;
 
 import java.util.List;
 
+/**
+ * The type Place result dto.
+ */
 public record PlaceResultDto(
         List<PlaceDto> placeDtoList,
         DevSiteCode devSiteCode
