093658e9c84085026eea23fd86dcd12bef723eac
jongjoo kim <jongjoo.kim@hanwha.com>
2022-08-29 20:14:07 +0900

    feat: javadoc

--- src/main/java/com/example/searchsample/common/config/QuerydslConfig.java
+++ src/main/java/com/example/searchsample/common/config/QuerydslConfig.java
@@ -7,14 +7,25 @@ import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+/**
+ * QueryDsl Config
+ */
 @Configuration
 public class QuerydslConfig {
     @PersistenceContext
     private EntityManager entityManager;
 
+    /**
+     * QueryDsl Config
+     */
     public QuerydslConfig() {
     }
 
+    /**
+     * QueryDsl Factory Bean
+     *
+     * @return the jpa query factory
+     */
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(this.entityManager);
